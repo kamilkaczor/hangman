a9662688b7f7aa7f34c0829f53214f8ec53b4f53
kam_kac <kaczor.kamil@gmail.com>
2022-06-07 07:22:06 +0000

    finish

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #Step 2
 
 import random
-word_list = ["aardvark", "baboon", "camel"]
+from hangman_words import word_list
+from hangman_art import stages, logo
+
+print(logo)
+
 chosen_word = random.choice(word_list)
 display = []
 for letter in chosen_word:
@@ -9,26 +13,29 @@ for letter in chosen_word:
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-while():
+end_of_game = False
+life = 6
+while(not end_of_game):
   guess = input("Guess a letter: ").lower()
+  if guess in display:
+   print(f"{guess} is already in word")
+   life -=1
+   print(stages[life])
+  for i in range(0, len(chosen_word)):
+    if chosen_word[i] == guess:
+      display[i]  = guess
+  print(display)
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+  if guess not in chosen_word:
+        print(stages[life])
+        life -= 1
+        if life == 0:
+          print(stages[life])
+          print("You loose a game")
+          end_of_game = True
+  if "_" not in display:
+    end_of_game = True
+    print("You won a game")
+    print(f"solution is:  {' '.join(display)}")
+      
 
- 
-  for i in range(0,len(chosen_word)):
-      if chosen_word.index(guess) == i:
-        display[i] = guess
-          
-
-
-
-  print (display)
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
